13b9b51b16b3376c5c27b52045c2d4aac73c0649
blank-27 <lcs2019045@iiitl.ac.in>
2021-02-05 00:09:49 +0530

    added light/dark functionality

--- js/home.js
+++ js/home.js
@@ -95,3 +95,25 @@ var fullAnimation = (function() {
 // 	  fullAnimation.seek()
 //   })
   
+
+
+//////////////////checkbox///////////////////
+
+var checkbox = document.querySelector('input[name=theme]');
+
+checkbox.addEventListener('change', function() {
+	if(this.checked) {
+		trans()
+		document.documentElement.setAttribute('data-theme', 'light')
+	} else {
+		trans()
+		document.documentElement.setAttribute('data-theme', 'dark')
+	}
+})
+
+let trans = () => {
+	document.documentElement.classList.add('transition');
+	window.setTimeout(() => {
+		document.documentElement.classList.remove('transition')
+	}, 1000)
+}
